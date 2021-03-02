6ca2f30446209f23ce67a83fe73cd7f4f2b66da3
Renaud Dumont <renaud.dumont@live.fr>
2021-03-02 18:56:18 +0100

    Added Swipe to approve on list items.

--- DemoApp/DemoApp/Models/Post.cs
+++ DemoApp/DemoApp/Models/Post.cs
@@ -1,11 +1,36 @@
-﻿namespace DemoApp.Models
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace DemoApp.Models
 {
-    public class Post
+    public class Post : ObservableObject
     {
         public long Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
-        
+
         public long UserId { get; set; }
+
+        private bool _read;
+
+        public bool Read
+        {
+            get { return _read; }
+            set { SetProperty(ref _read, value); }
+        }
+
+        public AsyncCommand OnApproveCommand { get; set; }
+
+        public Post()
+        {
+            OnApproveCommand = new AsyncCommand(OnApprove, allowsMultipleExecutions : false);
+        }
+
+        private async Task OnApprove()
+        {
+            await Task.Delay(250);
+            Read = true;
+        }
     }
 }
